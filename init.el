e0bffd91d76b9e96c067275e508cd05c07a2f413
Kevin Borling <kborling@protonmail.com>
2025-04-03 20:33:16 -0400

    eglot sqls

--- init.el
+++ init.el
@@ -666,6 +666,54 @@ If point is at the end of the line, kill the whole line including the newline."
    eglot-ignored-server-capabilities '(:hoverProvider
                                        :documentHighlightProvider))
 
+  (add-to-list 'display-buffer-alist
+               '("\\*sqls\\*"
+                 (display-buffer-reuse-window display-buffer-at-bottom)
+                 (reusable-frames . visible)
+                 (window-height . 0.3)))
+
+  (defclass eglot-sqls (eglot-lsp-server) () :documentation "SQL's Language Server")
+  (add-to-list 'eglot-server-programs '(sql-mode . (eglot-sqls "sqls")))
+  (cl-defmethod eglot-execute-command
+    ((server eglot-sqls) (command (eql executeQuery)) arguments)
+    "For executeQuery."
+    ;; (ignore-errors
+    (let* ((beg (eglot--pos-to-lsp-position (if (use-region-p) (region-beginning) (point-min))))
+           (end (eglot--pos-to-lsp-position (if (use-region-p) (region-end) (point-max))))
+           (res (jsonrpc-request server :workspace/executeCommand
+                                 `(:command ,(format "%s" command) :arguments ,arguments
+                                            :timeout 0.5 :range (:start ,beg :end ,end))))
+           (buffer (generate-new-buffer "*sqls*")))
+      (with-current-buffer buffer
+        (eglot--apply-text-edits `[
+                                   (:range
+                                    (:start
+                                     (:line 0 :character 0)
+                                     :end
+                                     (:line 0 :character 0))
+                                    :newText ,res)
+                                   ]
+                                 )
+        (org-mode))
+      (pop-to-buffer buffer))
+    )
+  (cl-defmethod eglot-execute-command
+    ((server eglot-sqls) (_cmd (eql switchDatabase)) arguments)
+    "For switchDatabase."
+    (let* ((res (jsonrpc-request server :workspace/executeCommand
+                                 `(:command "showDatabases" :arguments ,arguments :timeout 0.5)))
+           (menu-items (split-string res "\n"))
+           (menu `("Eglot code actions:" ("dummy" ,@menu-items)))
+           (db (if (listp last-nonmenu-event)
+                   (x-popup-menu last-nonmenu-event menu)
+                 (completing-read "[eglot] Pick an database: "
+                                  menu-items nil t
+                                  nil nil (car menu-items))
+                 ))
+           )
+      (jsonrpc-request server :workspace/executeCommand
+                       `(:command "switchDatabase" :arguments [,db] :timeout 0.5))))
+
   ;; Language Servers
   (add-to-list 'eglot-server-programs '(csharp-mode . ("csharp-ls")))
   (add-to-list 'eglot-server-programs '(typescript-ts-mode . ("typescript-language-server" "--stdio")))
@@ -1071,24 +1119,6 @@ If point is at the end of the line, kill the whole line including the newline."
   :config
   (add-hook 'eshell-load-hook #'eat-eshell-mode))
 
-;; Dape =========================================== ;;
-
-(use-package dape
-  :preface
-  :hook
-  (kill-emacs . dape-breakpoint-save)
-  (after-init . dape-breakpoint-load)
-  :config
-  (dape-breakpoint-global-mode)
-  (setq dape-buffer-window-arrangement 'right
-        dape-info-hide-mode-line nil
-        dape-inlay-hints t)
-
-  ;; Save buffers on startup, useful for interpreted languages
-  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
-  ;; Kill compile buffer on build success
-  (add-hook 'dape-compile-hook 'kill-buffer))
-
 ;; Org Mode ===================================== ;;
 
 ;;; Org Mode
@@ -1188,6 +1218,7 @@ If point is at the end of the line, kill the whole line including the newline."
   :config
   (defun meow-setup ()
     (setq meow-cheatsheet-layout meow-cheatsheet-layout-dvorak)
+    (setq meow-use-enhanced-selection-effect t)
     (meow-leader-define-key
      '("1" . meow-digit-argument)
      '("2" . meow-digit-argument)
