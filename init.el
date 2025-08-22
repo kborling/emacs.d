4286ac684540c1ff63bc5e7adac26661efa103de
Kevin Borling <kborling@protonmail.com>
2025-08-22 07:26:31 -0400

    Add winum

--- init.el
+++ init.el
@@ -1185,6 +1185,22 @@ If point is at the end of the line, kill the whole line including the newline."
   :ensure t
   :hook (after-init . hyperbole-mode))
 
+;; Winum - Window Numbers ========================== ;;
+
+(use-package winum
+  :ensure t
+  :config
+  (winum-mode)
+  :bind (("M-1" . winum-select-window-1)
+         ("M-2" . winum-select-window-2)
+         ("M-3" . winum-select-window-3)
+         ("M-4" . winum-select-window-4)
+         ("M-5" . winum-select-window-5)
+         ("M-6" . winum-select-window-6)
+         ("M-7" . winum-select-window-7)
+         ("M-8" . winum-select-window-8)
+         ("M-9" . winum-select-window-9)))
+
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; no-native-compile: t
