55d1077fcae7e567397ae612f6f790b401891296
Kevin <kborling@protonmail.com>
2026-07-02 19:29:49 -0400

    Remove unused init-encryption.el, update custom.el package list

--- custom.el
+++ custom.el
@@ -6,15 +6,15 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(acme-theme agent-shell angular-mode async auto-sudoedit cape
-                claude-code combobulate corfu deadgrep deferred deft
-                dotnet eat eglot-booster exec-path-from-shell exwm
-                fleury-theme flycheck fussy geiser-guile gptel guix
-                helpful hl-todo marginalia markdown-mode memmet-mode
-                multi-vterm multiple-cursors oauth2 orderless
-                org-appear org-modern rust-mode simple-httpd sudo-edit
-                tempel-collection treesit-auto treesit-expand
-                uwu-theme vc-msg web-server websocket winum
-                with-editor zig-mode))
+                claude-code combobulate corfu csv-mode deadgrep
+                deferred deft dotnet eat eglot-booster
+                exec-path-from-shell exwm fleury-theme flycheck fussy
+                geiser-guile gptel guix helpful hl-todo marginalia
+                markdown-mode memmet-mode multi-vterm multiple-cursors
+                oauth2 orderless org-appear org-modern rust-mode
+                simple-httpd sudo-edit tempel-collection treesit-auto
+                treesit-expand uwu-theme vc-msg web-server websocket
+                winum with-editor zig-mode))
  '(package-vc-selected-packages
    '((fleury-theme :url "https://github.com/kborling/fleury-theme.el")
      (eat :url "https://codeberg.org/akib/emacs-eat")
